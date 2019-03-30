1d7725716a00735a457f0f9d0d8b1c78ea4b777e
drobinson775 <47909306+drobinson775@users.noreply.github.com>
2019-03-29 21:01:27 -0400

    Update Suggestion.java

--- ClueLess/src/clueless/Suggestion.java
+++ ClueLess/src/clueless/Suggestion.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clueless;
 
 /**
- *
+ * The suggestion class handles all functionality related to making a suggestion.
  * 
  */
 public class Suggestion {
