7b6df070417c9a9d48a88323af3644c447aca4ac
drobinson775 <47909306+drobinson775@users.noreply.github.com>
2019-03-29 21:02:40 -0400

    Update Player.java

--- ClueLess/src/clueless/Player.java
+++ ClueLess/src/clueless/Player.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clueless;
 
 /**
- *
- * @author drobi
+ * The player class handles all functionality related to a player.
+ * 
  */
 public class Player {
    
