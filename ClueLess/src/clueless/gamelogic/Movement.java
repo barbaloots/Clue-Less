45562f1a911185193865d66184826098fa23d2d7
mahonkh <47911087+mahonkh@users.noreply.github.com>
2019-03-31 13:21:50 -0700

    Update Movement.java

--- ClueLess/src/clueless/gamelogic/Movement.java
+++ ClueLess/src/clueless/gamelogic/Movement.java
@@ -1,9 +1,18 @@
 package clueless.gamelogic;
 
 /**
- *
- * 
+ * This class represents the movement from players and weapons
+ * each turn of the game. Every turn, there is player movement
+ * caused by the turn player for their character as well as player
+ * movement on the other characters caused by suggestions. Weapons
+ * are also moved by means of suggestion, and this class enables
+ * that they move properly.
  */
-public class Movement {
-   
+public class Movement 
+{
+    private Location turnPlayerCurrentLocation;
+    private Location destinationLocation;
+    private String direction;
+    private int spacesToMove;
+    private boolean validMove;
 }
