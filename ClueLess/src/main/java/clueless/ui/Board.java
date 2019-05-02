dbab34cadd285b119f6971583bf582d6d7b73531
erinsmedley <erinsmedley@erins-air.home>
2019-05-02 19:14:47 -0400

    Added game pieces starting positions to the board GUI

--- ClueLess/src/main/java/clueless/ui/Board.java
+++ ClueLess/src/main/java/clueless/ui/Board.java
@@ -10,13 +10,16 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import clueless.gamelogic.Game;
 import clueless.gamelogic.Player;
 import clueless.gamelogic.TurnEnforcement;
+import clueless.gamelogic.locationenums.LocationEnum;
 
 /**
  * Class for general/main board UI and user interfacing
@@ -31,6 +34,16 @@ public class Board {
 	private Player player;
 	private Game game;
 	
+	private JFrame board;
+	private JPanel panel;
+	
+	private Image greenPiece;
+	private Image mustardPiece;
+	private Image peacockPiece;
+	private Image plumPiece;
+	private Image scarletPiece;
+	private Image whitePiece;
+	
 	/**
 	 * Constructor
 	 */
@@ -39,10 +52,11 @@ public class Board {
 		this.player = player;
 		
 		init();
+		initGamePieces();
 	}
 	
 	private void init() {
-		JFrame board = new JFrame();
+		board = new JFrame();
 		board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		board.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		board.setTitle("Bar-Ba-Loots Clue-Less");
@@ -51,7 +65,7 @@ public class Board {
 		
 		try {
 			backgroundImage = ImageIO.read(new File("ClueLess/src/main/Resources/ClueBoard.png"));
-			JPanel panel = new JPanel(new BorderLayout()) {
+			panel = new JPanel(new BorderLayout()) {
 		
 				@Override
 				public void paintComponent(Graphics g) {
@@ -60,7 +74,7 @@ public class Board {
 		
 			};
 
-			panel.setLayout(new FlowLayout());
+			JPanel buttonsPanel = new JPanel(new FlowLayout());
 			
 			JButton handButton = new JButton("SHOW HAND");
 			handButton.addActionListener(new ActionListener() {
@@ -104,11 +118,13 @@ public class Board {
 				}
 			});
 			
-			panel.add(handButton);
-			panel.add(notebookButton);
-			panel.add(endTurnButton);
-			panel.add(suggestButton);
-			panel.add(accuseButton);
+			buttonsPanel.add(handButton);
+			buttonsPanel.add(notebookButton);
+			buttonsPanel.add(endTurnButton);
+			buttonsPanel.add(suggestButton);
+			buttonsPanel.add(accuseButton);
+			
+			panel.add(buttonsPanel, BorderLayout.NORTH);
 			
 			board.add(panel);
 			
@@ -119,5 +135,45 @@ public class Board {
 		
 		board.pack();
 		board.setVisible(true);
+	}
+	
+	private void initGamePieces() {
+		try {
+			greenPiece = ImageIO.read(new File("ClueLess/src/main/Resources/GamePieces/GreenPiece.png"));
+			mustardPiece = ImageIO.read(new File("ClueLess/src/main/Resources/GamePieces/MustardPiece.png"));
+			peacockPiece = ImageIO.read(new File("ClueLess/src/main/Resources/GamePieces/PeacockPiece.png"));			
+			plumPiece = ImageIO.read(new File("ClueLess/src/main/Resources/GamePieces/PlumPiece.png"));
+			scarletPiece = ImageIO.read(new File("ClueLess/src/main/Resources/GamePieces/ScarletPiece.png"));	
+			whitePiece = ImageIO.read(new File("ClueLess/src/main/Resources/GamePieces/WhitePiece.png"));
+			
+			JPanel piecesPanel = new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+					//g.drawImage(greenPiece, LocationEnum.CONSERVATORYTOBALLROOMHALLWAY.getLocation().getX(), LocationEnum.CONSERVATORYTOBALLROOMHALLWAY.getLocation().getY(), null);
+					//g.drawImage(mustardPiece, LocationEnum.LOUNGETODININGROOMHALLWAY.getLocation().getX(), LocationEnum.LOUNGETODININGROOMHALLWAY.getLocation().getY(), null);
+					//g.drawImage(peacockPiece, LocationEnum.LIBRARYTOCONSERVATORYHALLWAY.getLocation().getX(), LocationEnum.LIBRARYTOCONSERVATORYHALLWAY.getLocation().getY(), null);
+					//g.drawImage(plumPiece, LocationEnum.STUDYTOLIBRARYHALLWAY.getLocation().getX(), LocationEnum.STUDYTOLIBRARYHALLWAY.getLocation().getY(), null);
+					//g.drawImage(scarletPiece, LocationEnum.HALLTOLOUNGEHALLWAY.getLocation().getX(), LocationEnum.HALLTOLOUNGEHALLWAY.getLocation().getY(), null);
+					//g.drawImage(whitePiece, LocationEnum.BALLROOMTOKITCHENHALLWAY.getLocation().getX(), LocationEnum.BALLROOMTOKITCHENHALLWAY.getLocation().getY(), null);
+					
+					g.drawImage(greenPiece, 566, 630, null);
+					g.drawImage(mustardPiece, 940, 230, null);
+					g.drawImage(peacockPiece, 437, 490, null);
+					g.drawImage(plumPiece, 437, 220, null);
+					g.drawImage(scarletPiece, 815, 90, null);
+					g.drawImage(whitePiece, 815, 630, null);
+				}
+			};
+			
+			panel.add(piecesPanel);
+			
+			panel.revalidate();
+			panel.repaint();
+			board.revalidate();
+			
+		} catch (IOException e) {
+			//TODO Audo-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
