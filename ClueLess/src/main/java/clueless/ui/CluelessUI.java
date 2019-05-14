22f3c2a8f7c868e4c0a6a6a512af9fb005be54ac
drobinson775 <47909306+drobinson775@users.noreply.github.com>
2019-05-14 13:29:46 -0400

    Update CluelessUI.java

--- ClueLess/src/main/java/clueless/ui/CluelessUI.java
+++ ClueLess/src/main/java/clueless/ui/CluelessUI.java
@@ -2,14 +2,14 @@ package clueless.ui;
 
 import clueless.gamelogic.Player;
 
+
 /**
  * Just a placeholder for now.
  * 
  * @author matthewsobocinski
  */
-/*public class CluelessUI {
-   public static void main(String[] args) {
-	   HomeScreen start = new HomeScreen();
-   }
-}*/
-
+public class CluelessUI {
+   /*public static void main(String[] args) {
+	   HomeScreen start = new HomeScreen(PrintWriter clientOut);
+   }*/
+}
