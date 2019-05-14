9f3b03935732dabf51b5644d4a2e8e77586618fa
drobinson775 <47909306+drobinson775@users.noreply.github.com>
2019-05-14 13:28:54 -0400

    Update Board.java

--- ClueLess/src/main/java/clueless/ui/Board.java
+++ ClueLess/src/main/java/clueless/ui/Board.java
@@ -3,11 +3,13 @@ package clueless.ui;
 
 import java.awt.BorderLayout;
 
+
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Dimension;
 import java.io.*;
 
 import javax.imageio.ImageIO;
@@ -33,7 +35,7 @@ import clueless.networking.*;
 public class Board {
 
 	private Image backgroundImage;
-	private Notebook notebook;
+	private NotebookUI notebook;
 	private Player player;
 	private Game game;
 	
@@ -64,6 +66,7 @@ public class Board {
 		board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		board.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		board.setTitle("Bar-Ba-Loots Clue-Less");
+		board.setResizable(false);
 
 
 		
@@ -77,6 +80,8 @@ public class Board {
 				}
 		
 			};
+			
+			panel.setPreferredSize(new Dimension(1424,777));
 
 			JPanel buttonsPanel = new JPanel(new FlowLayout());
 			
@@ -91,7 +96,7 @@ public class Board {
 			notebookButton.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					if (notebook == null) {
-						notebook = new Notebook(notebookButton, player);
+						notebook = new NotebookUI(notebookButton, player);
 						notebook.open(player);
 					}
 					else {
@@ -103,8 +108,9 @@ public class Board {
 			JButton endTurnButton = new JButton("END TURN");
 			endTurnButton.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					TurnEnforcement.turnMade();
-					endTurnButton.setEnabled(false);
+					clientOut.println("Done");
+					//TurnEnforcement.turnMade();
+					//endTurnButton.setEnabled(false);
 				}
 			});
 			
@@ -122,11 +128,19 @@ public class Board {
 				}
 			});
 			
+			JButton move = new JButton("MOVE");
+			move.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					new MoveWindow(board, clientOut, player);
+				}
+			});
+			
 			buttonsPanel.add(handButton);
 			buttonsPanel.add(notebookButton);
 			buttonsPanel.add(endTurnButton);
 			buttonsPanel.add(suggestButton);
 			buttonsPanel.add(accuseButton);
+			buttonsPanel.add(move);
 			
 			panel.add(buttonsPanel, BorderLayout.NORTH);
 			
@@ -142,6 +156,119 @@ public class Board {
 	}
 	
 	private void initGamePieces() {
+		try {
+			greenPiece = ImageIO.read(new File("src/main/Resources/GamePieces/GreenPiece.png"));
+			mustardPiece = ImageIO.read(new File("src/main/Resources/GamePieces/MustardPiece.png"));
+			peacockPiece = ImageIO.read(new File("src/main/Resources/GamePieces/PeacockPiece.png"));			
+			plumPiece = ImageIO.read(new File("src/main/Resources/GamePieces/PlumPiece.png"));
+			scarletPiece = ImageIO.read(new File("src/main/Resources/GamePieces/ScarletPiece.png"));	
+			whitePiece = ImageIO.read(new File("src/main/Resources/GamePieces/WhitePiece.png"));
+			
+			String playerAbbreviation = player.getAbbreviation();
+			
+			/*switch(playerAbbreviation)
+			{
+			    case "PP": g.drawImage(plumPiece, MovementEnum.STUDYTOLIBRARYHALLWAY.getLocation().getX(), MovementEnum.STUDYTOLIBRARYHALLWAY.getLocation().getY(), null);
+			    break;
+			    case "CM": g.drawImage(mustardPiece, MovementEnum.LOUNGETODININGROOMHALLWAY.getLocation().getX(), MovementEnum.LOUNGETODININGROOMHALLWAY.getLocation().getY(), null);
+			    break;
+			    case "MS": 
+			    	
+			}*/
+			
+			/*JPanel piecesPanel = new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+					g.drawImage(greenPiece, MovementEnum.CONSERVATORYTOBALLROOMHALLWAY.getLocation().getX(), MovementEnum.CONSERVATORYTOBALLROOMHALLWAY.getLocation().getY(), null);
+					g.drawImage(mustardPiece, MovementEnum.LOUNGETODININGROOMHALLWAY.getLocation().getX(), MovementEnum.LOUNGETODININGROOMHALLWAY.getLocation().getY(), null);
+					g.drawImage(peacockPiece, MovementEnum.LIBRARYTOCONSERVATORYHALLWAY.getLocation().getX(), MovementEnum.LIBRARYTOCONSERVATORYHALLWAY.getLocation().getY(), null);
+					g.drawImage(plumPiece, MovementEnum.STUDYTOLIBRARYHALLWAY.getLocation().getX(), MovementEnum.STUDYTOLIBRARYHALLWAY.getLocation().getY(), null);
+					g.drawImage(scarletPiece, MovementEnum.HALLTOLOUNGEHALLWAY.getLocation().getX(), MovementEnum.HALLTOLOUNGEHALLWAY.getLocation().getY(), null);
+					g.drawImage(whitePiece, MovementEnum.BALLROOMTOKITCHENHALLWAY.getLocation().getX(), MovementEnum.BALLROOMTOKITCHENHALLWAY.getLocation().getY(), null);					
+				}
+			};*/
+			
+			JPanel greenPiecePanel = new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+					g.drawImage(greenPiece, MovementEnum.CONSERVATORYTOBALLROOMHALLWAY.getLocation().getX(), MovementEnum.CONSERVATORYTOBALLROOMHALLWAY.getLocation().getY(), null);
+				
+				}
+			};
+			
+			//panel.add(greenPiecePanel);
+			board.add(greenPiecePanel);
+			
+			JPanel mustardPiecePanel = new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+
+					g.drawImage(mustardPiece, MovementEnum.LOUNGETODININGROOMHALLWAY.getLocation().getX(), MovementEnum.LOUNGETODININGROOMHALLWAY.getLocation().getY(), null);
+				
+				}
+			};
+			
+			//panel.add(mustardPiecePanel);
+			board.add(mustardPiecePanel);
+			
+			JPanel peacockPiecePanel = new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+
+					g.drawImage(peacockPiece, MovementEnum.LIBRARYTOCONSERVATORYHALLWAY.getLocation().getX(), MovementEnum.LIBRARYTOCONSERVATORYHALLWAY.getLocation().getY(), null);
+				
+				}
+			};
+			
+			//panel.add(peacockPiecePanel);
+			board.add(peacockPiecePanel);
+			
+			JPanel plumPiecePanel = new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+
+					g.drawImage(plumPiece, MovementEnum.STUDYTOLIBRARYHALLWAY.getLocation().getX(), MovementEnum.STUDYTOLIBRARYHALLWAY.getLocation().getY(), null);
+				
+				}
+			};
+			
+			//panel.add(plumPiecePanel);
+			board.add(plumPiecePanel);
+			
+			JPanel scarletPiecePanel = new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+
+					g.drawImage(scarletPiece, MovementEnum.HALLTOLOUNGEHALLWAY.getLocation().getX(), MovementEnum.HALLTOLOUNGEHALLWAY.getLocation().getY(), null);
+				
+				}
+			};
+			
+			//panel.add(scarletPiecePanel);
+			board.add(scarletPiecePanel);
+			
+			JPanel whitePiecePanel = new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+
+					g.drawImage(whitePiece, MovementEnum.BALLROOMTOKITCHENHALLWAY.getLocation().getX(), MovementEnum.BALLROOMTOKITCHENHALLWAY.getLocation().getY(), null);					
+				}
+			};
+			
+			//panel.add(whitePiecePanel);
+			board.add(whitePiecePanel);
+			
+			panel.revalidate();
+			panel.repaint();
+			board.revalidate();
+			
+		} catch (IOException e) {
+			//TODO Audo-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void updateGamePieces(String movement) {
 		try {
 			greenPiece = ImageIO.read(new File("src/main/Resources/GamePieces/GreenPiece.png"));
 			mustardPiece = ImageIO.read(new File("src/main/Resources/GamePieces/MustardPiece.png"));
