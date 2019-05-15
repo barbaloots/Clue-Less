ecb8d70adb1179e19e17423ef26caacf6a86fc0c
erinsmedley <erinsmedley@erins-air.home>
2019-05-14 21:53:32 -0400

    Fixed game piece locations

--- ClueLess/src/main/java/clueless/gamelogic/locationenums/MovementEnum.java
+++ ClueLess/src/main/java/clueless/gamelogic/locationenums/MovementEnum.java
@@ -8,36 +8,36 @@ import clueless.gamelogic.Location;
  *
  */
 public enum MovementEnum {
-		// First row for GUI placement
-		STUDY(new Location(437,90)),
-		STUDYTOHALLHALLWAY(new Location(566,90)),
-		HALL(new Location(685,90)),
-		HALLTOLOUNGEHALLWAY(new Location(815, 90)),
-		LOUNGE(new Location(940,90)),
-		
-		// Second row for GUI placement
-		STUDYTOLIBRARYHALLWAY(new Location(437, 220)),
-		HALLTOBILLIARDROOMALLWAY(new Location(685,220)),
-		LOUNGETODININGROOMHALLWAY(new Location(940, 230)),
-		
-		// Third row for GUI placement
-		LIBRARY(new Location(437,360)),
-		LIBRARYTOBILLIARDROOMHALLWAY(new Location(566, 360)),
-		BILLIARDROOM(new Location(685,360)),
-		BILLIARDROOMTODININGROOMHALLWAY(new Location(815,360)),
-		DININGROOM(new Location(940,360)),
+	// First row for GUI placement
+	STUDY(new Location(177,90)),
+	STUDYTOHALLHALLWAY(new Location(306,90)),
+	HALL(new Location(425,90)),
+	HALLTOLOUNGEHALLWAY(new Location(555, 90)),
+	LOUNGE(new Location(680,90)),
+	
+	// Second row for GUI placement
+	STUDYTOLIBRARYHALLWAY(new Location(177, 220)),
+	HALLTOBILLIARDROOMALLWAY(new Location(425,220)),
+	LOUNGETODININGROOMHALLWAY(new Location(680, 230)),
+	
+	// Third row for GUI placement
+	LIBRARY(new Location(177,360)),
+	LIBRARYTOBILLIARDROOMHALLWAY(new Location(306, 360)),
+	BILLIARDROOM(new Location(425,360)),
+	BILLIARDROOMTODININGROOMHALLWAY(new Location(555,360)),
+	DININGROOM(new Location(680,360)),
 
-		// Fourth row for GUI placement
-		LIBRARYTOCONSERVATORYHALLWAY(new Location(437, 490)),
-		BILLIARDROOMTOBALLROOMHALLWAY(new Location(685,490)),
-		DININGROOMTOKITCHENHALLWAY(new Location(940,490)),
-		
-		// Fifth row for GUI placement
-		CONSERVATORY(new Location(437,630)),
-		CONSERVATORYTOBALLROOMHALLWAY(new Location(566, 630)),
-		BALLROOM(new Location(685,630)),
-		BALLROOMTOKITCHENHALLWAY(new Location(815, 630)),
-		KITCHEN(new Location(940,630));
+	// Fourth row for GUI placement
+	LIBRARYTOCONSERVATORYHALLWAY(new Location(177, 490)),
+	BILLIARDROOMTOBALLROOMHALLWAY(new Location(425,490)),
+	DININGROOMTOKITCHENHALLWAY(new Location(680,490)),
+	
+	// Fifth row for GUI placement
+	CONSERVATORY(new Location(177,630)),
+	CONSERVATORYTOBALLROOMHALLWAY(new Location(306, 630)),
+	BALLROOM(new Location(425,630)),
+	BALLROOMTOKITCHENHALLWAY(new Location(555, 630)),
+	KITCHEN(new Location(680,630));
 		
 		Location location;
 		
